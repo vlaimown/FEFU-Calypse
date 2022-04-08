e7c4e9a661ec19bafdc4915c7d3d98bc03aaa813
vlaimown <v1ad5tepanov@mail.ru>
2022-04-08 15:37:37 +1000

    Начало диалоговой системы

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,44 +1,119 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+
+    [SerializeField] private DialoguesController dialoguesController;
+
     [SerializeField] private Rigidbody2D hero;
     [SerializeField] private Animator animator;
     [SerializeField] private float speed;
 
     [SerializeField] private Vector2 direction;
 
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float attackRange = 0.5f;
+
+    public GameObject firstDialogue;
+
+    public float maxspeed;
+
+    public int flag;
+
+    public LayerMask enemyLayers;
+
+    public bool attackEnable;
+    public float cooldown;
+    public float maxcooldown;
+
     private bool facingRight = true;
 
     private void Start()
     {
-        hero = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-    }
-    private void Update()
-    {
-        direction = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        animator.SetFloat("Horizontal", direction.x);
-        animator.SetFloat("Vertical", direction.y);
-        animator.SetFloat("Speed", direction.sqrMagnitude);
+        speed = maxspeed;
+        attackEnable = true;
+
+        flag = 0;
+
+        maxcooldown = 1f;
+        cooldown = maxcooldown;
     }
 
     private void FixedUpdate()
     {
-        hero.MovePosition(hero.position + direction * speed * Time.deltaTime);
-
-        if(facingRight == true && direction.x < 0)
+        if (dialoguesController.startFlag == 1)
         {
-            Flip();
+
+            direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+            animator.SetFloat("Horizontal", direction.x);
+            animator.SetFloat("Vertical", direction.y);
+            animator.SetFloat("Speed", direction.sqrMagnitude);
+
+            if (cooldown > 0 && flag == 1)
+            {
+                cooldown -= Time.deltaTime;
+                attackEnable = false;
+            }
+            else if (cooldown <= 0)
+            {
+                attackEnable = true;
+                cooldown = maxcooldown;
+                flag = 0;
+            }
+
+            hero.MovePosition(hero.position + direction * speed * Time.deltaTime);
+
+            if (facingRight == true && direction.x < 0)
+            {
+                Flip();
+            }
+
+            else if (facingRight == false && direction.x > 0)
+            {
+                Flip();
+            }
+
+            if (Input.GetButtonDown("Fire1") && (attackEnable == true) && (cooldown == 1) && flag == 0)
+            {
+                Attack();
+            }
+        }
+    }
+
+    private void Attack()
+    {
+        speed = 0;
+
+        animator.SetBool("IsAttacking", true);
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+        }
+    }
+
+    private void ReturnAttack()
+    {
+        animator.SetBool("IsAttacking", false);
+        attackEnable = false;
+        speed = maxspeed;
+        flag = 1;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
         }
 
-        else if (facingRight == false && direction.x > 0)
-        {
-            Flip();
-        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 
     private void Flip()
@@ -50,77 +125,3 @@ public class PlayerController : MonoBehaviour
         transform.localScale = scaler;
     }
 }
-
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerController : MonoBehaviour
-{
-    public float speed;
-    public float health;
-    //public Animator animator;
-    private Vector2 direction;
-    private Rigidbody2D rb;
-
-    private bool facingRight = true;
-
-    private bool IsAttack = false;
-
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        //animator = GetComponent<Animator>();
-    }
-
-    void Update()
-    {
-
-        if (!IsAttack)
-        {
-            direction.x = Input.GetAxisRaw("Horizontal");
-            direction.y = Input.GetAxisRaw("Vertical");
-
-            //Debug.Log("Attack");
-            //animator.SetFloat("Horizontal", direction.x);
-            //animator.SetFloat("Vertical", direction.y);
-            //animator.SetFloat("Speed", direction.sqrMagnitude);
-            //Debug.Log("Attack");
-            if (Input.GetMouseButton(0))
-            {
-
-                //animator.SetTrigger("attack");
-                IsAttack = true;
-            }
-            
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
-
-        if (!facingRight && direction.x > 0)
-        {
-            Flip();
-        }
-        else if (facingRight && direction.x < 0)
-        {
-            Flip();
-        }
-    }
-    private void Flip()
-    {
-        facingRight = !facingRight;
-        Vector3 Scaler = transform.localScale;
-        Scaler.x *= -1;
-        transform.localScale = Scaler;
-    }
-
-    public void AttackOver()
-    {
-        IsAttack = false;
-
-    }
-}*/
